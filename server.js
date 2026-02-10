3f2279f8d5dbba0f433c2d93b0474ef6d304bcaf
ChrisSouthey <chsouthey@email.neit.edu>
2026-02-09 19:16:18 -0500

    Week 2 assignment

--- server.js
+++ server.js
@@ -1,40 +1,105 @@
+//Assignment 1 Stuff
 const express = require("express"); 
 const app = express(); 
 const path = require('path');
 const fs = require('fs');
-const PORT = 3000; 
+//Assignment #2 stuff
+const mongoose = require('mongoose');
+const strict = require('assert/strict');
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
+if (!MONGO_URI) {
+    console.error("Missing Connection Data");
+    process.exit(1);
+}
 
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.json());
+
+
+//Connects to MongoDB
+async function connectToMongo() {
+    try{
+        await mongoose.connect(MONGO_URI);
+        console.log("Connected to MongoDB!!");
+    }
+    catch (err){
+        console.error("Error connecting to MongoDB:", err.message);
+        process.exit(1);
+}};
+
 //Standard routes
-app.get("/index", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    console.log("Reached Index");
+// app.get("/index", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//     console.log("Reached Index");
+// });
+
+// app.get("/todo", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'todo.html'));
+//     console.log("Reached Todo");
+// });
+
+// app.get("/readtodo", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'readtodo.html'));
+//     console.log("Reached Read-Todo");
+// });
+
+
+//API route for data
+// app.get("/api/todo", (req, res) => {
+//     fs.readFile("todo.json", "utf8", (err, data) => {
+//         if (err){
+//             res.status(500).send("Error reading data file");
+//         };
+//         res.json(JSON.parse(data));
+//     });
+// });
+
+
+//Mongoose Schema and Model
+const todos = new mongoose.Schema({}, { strict: false }); 
+const Todos = mongoose.model('todos', todos);
+
+//Routes to stuff
+app.get("/api/todo/:id", async (req, res) => {
+    console.log(req.params.id);
+    const tinfo = req.params.id;
+    const todoInfo = await Todos.findOne({id: Number(tinfo)});
+    console.log(todoInfo);
+    res.json(todoInfo);
 });
 
-app.get("/todo", (req, res) => {
+app.get("/api/todo", async (req, res) => {
+    const data = await Todos.find();
+    res.setHeader("Content-Type", "application/json");
+    res.json(data);
+});
+
+app.get("/todo", async (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'todo.html'));
-    console.log("Reached Todo");
 });
 
-app.get("/readtodo", (req, res) => {
+app.get("/read-todo", async (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'readtodo.html'));
-    console.log("Reached Read-Todo");
+});
+
+app.get("/index", async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use((req, res) => {
+    res.redirect(301, "/index");
 });
 
 
-//API route fot data
-app.get("/api/todo", (req, res) => {
-    fs.readFile("todo.json", "utf8", (err, data) => {
-        if (err){
-            res.status(500).send("Error reading data file");
-        };
-        res.json(JSON.parse(data));
+//Run Server :D
+connectToMongo().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
     });
 });
-
-//Run Server
-app.get("/", (req, res) => { res.send("Express server is running!"); }); app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
